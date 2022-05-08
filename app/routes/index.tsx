9022f44f4e16a272118737e666b25ddbeaaa0c05
Jibin George <hello@jibingeo.com>
2022-05-08 14:49:04 +0530

    Added vercel-id

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,7 +1,20 @@
+import type { LoaderFunction } from "@remix-run/node";
+import { json } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
+
+export const loader: LoaderFunction = async ({ request }) => {
+  const vercelId = request.headers.get("x-vercel-id");
+  return json({
+    vercelId,
+  });
+};
+
 export default function Index() {
+  const { vercelId } = useLoaderData();
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-      <h1>Welcome to Remix</h1>
+      <h1>Products</h1>
+      <div>{vercelId}</div>
     </div>
   );
 }

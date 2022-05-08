c52f91c534746a621147ece2ec67c00d14dac9c2
Jibin George <hello@jibingeo.com>
2022-05-08 12:11:29 +0530

    Added link

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -26,6 +26,11 @@ export default function Index() {
             Remix Docs
           </a>
         </li>
+        <li>
+          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
+            Remix Docs
+          </a>
+        </li>
       </ul>
     </div>
   );

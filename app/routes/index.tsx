4dcbd14354dd7f88b68f9f5d6da75b9ca2a1d577
Jibin George <hello@jibingeo.com>
2022-05-08 12:37:04 +0530

    Fix

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -26,11 +26,6 @@ export default function Index() {
             Remix Docs
           </a>
         </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
       </ul>
     </div>
   );

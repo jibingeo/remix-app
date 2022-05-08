8a669bc17abdf4584f856befc498b2ae60096d7b
Jibin George <hello@jibingeo.com>
2022-05-08 21:03:22 +0530

    Added paging

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,17 +1,20 @@
 import type { LoaderFunction } from "@remix-run/node";
 import { json } from "@remix-run/node";
-import { useLoaderData } from "@remix-run/react";
+import { useLoaderData, Link } from "@remix-run/react";
 
 export async function loader({ request }) {
-  let apiUrl = "https://api.themoviedb.org/3/trending/all/day";
+  const url = new URL(request.url);
+  const page = Number(url.searchParams.get("page") || 1);
+  let apiUrl = `https://api.themoviedb.org/3/trending/all/day?page=${page}`;
   let res = await fetch(apiUrl, {
     headers: {
       Authorization: `Bearer ${process.env.TMDB_API_TOKEN}`,
     },
   });
-
-  let { results } = await res.json();
-
+  let { results, success, status_message } = await res.json();
+  if (success === false) {
+    throw new Error(status_message);
+  }
   let prunedData = results.map(({ id, title, poster_path }) => {
     return {
       id,
@@ -19,18 +22,23 @@ export async function loader({ request }) {
       poster_path,
     };
   });
-  return json(prunedData);
+  return json({
+    movies: prunedData,
+    nextPage: page + 1,
+  });
 }
 
 export default function Index() {
-  const movies = useLoaderData();
+  const { movies, nextPage } = useLoaderData();
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-      <div className="p-4 drop-shadow-md bg-white">
-        <img
-          src="https://www.themoviedb.org/assets/2/v4/logos/v2/blue_short-8e7b30f73a4020692ccca9c88bafe5dcb6f8a62a4c6bc55cd9ba82bb2cd95f6c.svg"
-          className="h-6 cursor-pointer"
-        />
+      <div className="drop-shadow-md bg-sky-900">
+        <div className="max-w-6xl m-auto p-4">
+          <img
+            src="https://www.themoviedb.org/assets/2/v4/logos/v2/blue_short-8e7b30f73a4020692ccca9c88bafe5dcb6f8a62a4c6bc55cd9ba82bb2cd95f6c.svg"
+            className="h-6 cursor-pointer"
+          />
+        </div>
       </div>
       <div className="max-w-6xl m-auto grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 p-4">
         {movies.map(({ id, title, poster_path }) => (
@@ -45,6 +53,14 @@ export default function Index() {
           </div>
         ))}
       </div>
+        <div className="max-w-6xl text-center m-auto p-4">
+      <Link to={`?page=${nextPage}`}>Next</Link>{" "}
+      </div>
     </div>
   );
+}
+
+export function ErrorBoundary({ error }) {
+  console.error(error);
+  return <div>{error.toString()}</div>;
 }
